d39a70cdc9d09cd529b2262703442531aa122ae0
pasatigiy <82204580+pasatigiy@users.noreply.github.com>
2021-05-24 15:05:34 +0300

    Add files via upload

--- src/Account.java
+++ src/Account.java
@@ -1,3 +1,19 @@
+/**
+ * Тут создан класс, в котором имеется статический метод.
+ * Этот метод принимает на вход три параметра:
+ *   login
+ *   password
+ *   confirmPassword
+ * Все поля имеют тип данных String.
+ * Длина login должна быть меньше 20 символов и не должен содержать пробелы.
+ * Если login не соответствует этим требованиям, необходимо выбросить WrongLoginException.
+ * Длина password должна быть меньше 20 символов, не должен содержать пробелом и должен содержать хотя бы одну цифру.
+ * Также password и confirmPassword должны быть равны.
+ * Если password не соответствует этим требованиям, необходимо выбросить WrongPasswordException.
+ * WrongPasswordException и WrongLoginException -
+ */
+
+
 public class Account {
     private String login;
     private String password;
